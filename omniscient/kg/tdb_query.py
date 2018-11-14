94ff8b5349b1125e2018cfbd347594b720c39c19
Impavidity <spone.2014@gmail.com>
2018-11-13 21:07:06 -0500

    clean up the lock after query

--- omniscient/kg/tdb_query.py
+++ omniscient/kg/tdb_query.py
@@ -1,8 +1,10 @@
 import argparse
+import os
 from jnius import autoclass
 
 class TDBQuery(object):
   def __init__(self, path):
+    self.path = path
     self.JString = autoclass('java.lang.String')
     self.JArray = autoclass("java.lang.reflect.Array")
     JTDBQuery = autoclass('botafogo/TDBQuery')
@@ -10,58 +12,50 @@ class TDBQuery(object):
 
   def query(self, qstring):
     results = self.tdbquery.query(self.JString(qstring))
-    return results
+    result_list = []
+    for i in range(results.size()):
+      query_solution = results.get(i)
+      solution_dict = {}
+      var_names = query_solution.varNames()
+      while var_names.hasNext():
+        var = var_names.next()
+        string = query_solution.get(var).toString()
+        solution_dict[var] = string
+      result_list.append(solution_dict)
+    return result_list
 
   def parallel_query(self, qstrings, num_threads):
     jqstrings = self.JArray.newInstance(self.JString, len(qstrings))
     for i in range(len(qstrings)):
       jqstrings[i] = self.JString(qstrings[i])
     results = self.tdbquery.parallelQuery(jqstrings, num_threads)
-    return results
+    parallel_result_list = []
+    for i in range(results.size()):
+      result = results.get(i)
+      result_list = []
+      for j in range(result.size()):
+        query_solution = result.get(j)
+        solution_dict = {}
+        iter = query_solution.varNames()
+        while iter.hasNext():
+          var = iter.next()
+          string = query_solution.get(var).toString()
+          solution_dict[var] = string
+        result_list.append(solution_dict)
+      parallel_result_list.append(result_list)
+    return parallel_result_list
+
+  def __del__(self):
+    os.remove(os.path.join(self.path, "journal.jrnl"))
+    os.remove(os.path.join(self.path, "tdb.lock"))
+
 
 if __name__ == "__main__":
   argparser = argparse.ArgumentParser()
   argparser.add_argument("--index")
   argparser.add_argument("--query")
   args = argparser.parse_args()
-  qstring = """SELECT ?s ?p ?o
-               WHERE { 
-                    ?s ?p ?o .
-                } LIMIT 10"""
-  tdbquery = TDBQuery(path=args.index)
-  print(qstring)
-  results = tdbquery.query(qstring)
-  for i in range(results.size()):
-    query_solution = results.get(i)
-    while query_solution.varNames().hasNext():
-      var = query_solution.varNames().next()
-      string = query_solution.get(var).toString()
-      print(var, string)
+  tdbquery = TDBQuery(args.index)
   print(args.query)
   results = tdbquery.query(args.query)
-  for i in range(results.size()):
-    query_solution = results.get(i)
-    while query_solution.varNames().hasNext():
-      var = query_solution.varNames().next()
-      string = query_solution.get(var).toString()
-      print(var, string)
-  # for i in range(results.size()):
-  #   query_solution = results.get(i)
-  #   iter = query_solution.varNames()
-  #   while iter.hasNext():
-  #     var = iter.next()
-  #     print(var, query_solution.get(var).toString())
-  #   print()
-  # qstrings = [qstring] * 10
-  # results = tdbquery.parallel_query(qstrings, 5)
-  # for i in range(results.size()):
-  #   result = results.get(i)
-  #   for j in range(result.size()):
-  #     query_solution = result.get(j)
-  #     iter = query_solution.varNames()
-  #     while iter.hasNext():
-  #       var = iter.next()
-  #       print(var, query_solution.get(var).toString())
-  #     print()
-  #   print()
-
+  print(results)
