53750eee82a4e5aec1e2f1bc4cf2e137130446fe
MichaelAzmy <michael.wahid@gmail.com>
2018-11-16 16:00:10 -0500

    Embeddings Indexer and Retriever

--- omniscient/embedding/indexer.py
+++ omniscient/embedding/indexer.py
@@ -2,28 +2,50 @@ import argparse
 import datetime
 import logging
 from sqlitedict import SqliteDict
+import numpy as np
+import os
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("EmbeddingIndexer")
+EMBEDDING = "embbeding_index"
 
-LOGGER = logging.getLogger("EmbeddingIndexer")
-LOGGER.setLevel(logging.INFO)
 
 class EmbeddingIndexer(object):
   def __init__(self, embedding_file, index_file):
+    print("SSS")
     self.embedding_file = embedding_file
     self.index_file = index_file
-    LOGGER.info("Input path: " + self.embedding_file)
-    LOGGER.info("Index path: " + self.index_file)
-    self.embedding = SqliteDict(self.index_file, autocommit=True)
+    logger.info("Input path: " + self.embedding_file)
+    logger.info("Index path: " + self.index_file)
+    self.embedding = SqliteDict(os.path.join(self.index_file, EMBEDDING), autocommit=True)
 
   def iterator(self):
-    raise NotImplementedError
+    with open(self.embedding_file) as f:
+      for line in f:
+        tokens = line.strip().split(" ")
+        if len(tokens) == 2:
+          continue
+        uri = tokens[0]
+        embedding = np.array(tokens[1:], dtype=np.float32)
+        yield (uri, embedding)
+
 
   def run(self):
     count = 0
     for key, value in self.iterator():
       self.embedding[key] = value
       count += 1
-      if count % 20000:
+      if count % 20000 == 0:
         self.embedding.commit()
-        LOGGER.info("[{}] {} index added.".format(datetime.datetime.now(), count))
+        logger.info("[{}] {} index added.".format(datetime.datetime.now(), count))
     self.embedding.close()
+
+def main(args):
+  EmbeddingIndexer(args.input, args.index).run()
+
+if __name__ == "__main__":
+  argparser = argparse.ArgumentParser()
+  argparser.add_argument("--input", type=str, required=True)
+  argparser.add_argument("--index", type=str, required=True)
+  args = argparser.parse_args()
+  main(args)
---
+++ omniscient/embedding/retriever.py
@@ -0,0 +1,28 @@
+import argparse
+import os
+from sqlitedict import SqliteDict
+import numpy as np
+
+from omniscient.embedding.indexer import EMBEDDING
+
+class Retriever(object):
+  def __init__(self, index_path):
+    self.index_path = index_path
+    self.embedding = SqliteDict(os.path.join(self.index_path, EMBEDDING), flag="r")
+    print("Finish Loading")
+
+  def get(self, uri):
+    return self.embedding[uri]
+
+
+def main(args):
+    print(Retriever(args.index).get(args.uri))
+
+if __name__ == "__main__":
+  argparser = argparse.ArgumentParser()
+  argparser.add_argument("--index", type=str, required=True)
+  argparser.add_argument("--uri", type=str, default=None)
+
+  args = argparser.parse_args()
+  main(args)
+  print('Finish')
