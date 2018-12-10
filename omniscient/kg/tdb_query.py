1bd05ae89537a7747e42c03b6256f3abcdbf439e
Impavidity <spone.2014@gmail.com>
2018-12-10 14:48:59 -0500

    Fix out of memory issue for batch query

--- omniscient/kg/tdb_query.py
+++ omniscient/kg/tdb_query.py
@@ -1,5 +1,6 @@
 import argparse
 from jnius import autoclass
+from omniscient.kg import constant
 
 class TDBQuery(object):
   def __init__(self, path):
@@ -45,24 +46,28 @@ class TDBQuery(object):
     return sequential_result_list
 
   def parallel_query(self, qstrings, num_threads):
-    jqstrings = self.JArray.newInstance(self.JString, len(qstrings))
-    for i in range(len(qstrings)):
-      jqstrings[i] = self.JString(qstrings[i])
-    results = self.tdbquery.parallelQuery(jqstrings, num_threads)
     parallel_result_list = []
-    for i in range(results.size()):
-      result = results.get(i)
-      result_list = []
-      for j in range(result.size()):
-        query_solution = result.get(j)
-        solution_dict = {}
-        iter = query_solution.varNames()
-        while iter.hasNext():
-          var = iter.next()
-          string = query_solution.get(var).toString()
-          solution_dict[var] = string
-        result_list.append(solution_dict)
-      parallel_result_list.append(result_list)
+    start_position = 0
+    while start_position < len(qstrings):
+      sub_qstrings = qstrings[start_position: start_position + constant.CHUNKING_SIZE]
+      jsub_qstrings = self.JArray.newInstance(self.JString, len(sub_qstrings))
+      for i in range(len(sub_qstrings)):
+        jsub_qstrings[i] = self.JString(sub_qstrings[i])
+      results = self.tdbquery.parallelQuery(jsub_qstrings, num_threads)
+      for i in range(results.size()):
+        result = results.get(i)
+        result_list = []
+        for j in range(result.size()):
+          query_solution = result.get(j)
+          solution_dict = {}
+          iter = query_solution.varNames()
+          while iter.hasNext():
+            var = iter.next()
+            string = query_solution.get(var).toString()
+            solution_dict[var] = string
+          result_list.append(solution_dict)
+        parallel_result_list.append(result_list)
+      start_position += constant.CHUNKING_SIZE
     return parallel_result_list
 
   def __del__(self):
